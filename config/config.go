0141637b47f84fda3db4f9b169441914c2dfae8d
HiChon <59905927+chon26909@users.noreply.github.com>
2023-08-24 22:27:23 +0700

    create config app

--- config/config.go
+++ config/config.go
@@ -1,1 +1,51 @@
 package config
+
+import (
+	"os"
+	"time"
+)
+
+func envPath() string {
+	if len(os.Args) > 0 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+type config struct {
+	app *app
+	db  *db
+	jwt *jwt
+}
+
+type app struct {
+	host         string
+	port         uint
+	name         string
+	version      string
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+	bodyLimit    int
+	fileLimit    int
+	gcpBucket    string
+}
+
+type db struct {
+	host           string
+	port           int
+	protocal       string
+	username       string
+	password       string
+	database       string
+	sslMode        string
+	maxConnections int
+}
+
+type jwt struct {
+	adminKey         string
+	secertKey        string
+	apiKey           string
+	accessExpiresAt  int
+	refreshExpiresAt int
+}
