46826f7859dad0f3c87637e2b243499b2103a5fb
HiChon <59905927+chon26909@users.noreply.github.com>
2023-08-26 20:31:23 +0700

    create connect database

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,1 +1,20 @@
-package migrations
+package database
+
+import (
+	"log"
+
+	"github.com/chon26909/e-commerce/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+func NewDatabase(config config.IDbConfig) *sqlx.DB {
+	db, err := sqlx.Connect("pgx", config.Url())
+	if err != nil {
+		log.Fatalf("connect db failed %v\n", err)
+	}
+
+	db.SetMaxOpenConns(config.MaxOpenConnections())
+
+	return db
+}
