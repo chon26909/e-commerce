239904588b2bf624deac7684bc1bbbea5c01cd27
HiChon <59905927+chon26909@users.noreply.github.com>
2023-08-26 20:30:39 +0700

    edit config

--- config/config.go
+++ config/config.go
@@ -1,23 +1,13 @@
 package config
 
 import (
-	"os"
+	"log"
+	"math"
+	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
 )
-
-func envPath() string {
-	if len(os.Args) > 0 {
-		return ".env"
-	} else {
-		return os.Args[1]
-	}
-}
-
-type IConfig interface {
-	App()
-	DB()
-	JWT()
-}
 
 type config struct {
 	app *app
@@ -25,33 +15,101 @@ type config struct {
 	jwt *jwt
 }
 
-type app struct {
-	host         string
-	port         uint
-	name         string
-	version      string
-	readTimeout  time.Duration
-	writeTimeout time.Duration
-	bodyLimit    int
-	fileLimit    int
-	gcpBucket    string
+func LoadConfig(path string) IConfig {
+
+	envMap, err := godotenv.Read(path)
+	if err != nil {
+		log.Fatalf("could not load config : %v", err)
+	}
+
+	return &config{
+		app: &app{
+			host: envMap["APP_HOST"],
+			port: func() int {
+				p, err := strconv.Atoi(envMap["APP_PORT"])
+				if err != nil {
+					log.Fatalf("load port error : %v", err)
+				}
+				return p
+			}(),
+			name:    envMap["APP_NAME"],
+			version: envMap["APP_VERSION"],
+			readTimeout: func() time.Duration {
+				t, err := strconv.Atoi(envMap["APP_READ_TIMEOUT"])
+				if err != nil {
+					log.Fatalf("load readTimeout error : %v", err)
+				}
+				return time.Duration(int64(t) * int64(math.Pow10(9)))
+			}(),
+			writeTimeout: func() time.Duration {
+				t, err := strconv.Atoi(envMap["APP_WRTIE_TIMEOUT"])
+				if err != nil {
+					log.Fatalf("load writeTimeout error : %v", err)
+				}
+				return time.Duration(int64(t) * int64(math.Pow10(9)))
+			}(),
+			bodyLimit: func() int {
+				b, err := strconv.Atoi(envMap["APP_BODY_LIMIT"])
+				if err != nil {
+					log.Fatalf("load bodyLimit error : %v", err)
+				}
+				return b
+			}(),
+			fileLimit: func() int {
+				f, err := strconv.Atoi(envMap["APP_FILE_LIMIT"])
+				if err != nil {
+					log.Fatalf("load fileLimit error : %v", err)
+				}
+				return f
+			}(),
+			gcpBucket: envMap["APP_GCP_BUCKET"],
+		},
+		db: &db{
+			host: envMap["DB_HOST"],
+			port: func() int {
+				p, err := strconv.Atoi(envMap["DB_PORT"])
+				if err != nil {
+					log.Fatalf("load host error : %v", err)
+				}
+				return p
+			}(),
+			protocal: envMap["DB_PROTOCAL"],
+			username: envMap["DB_USERNAME"],
+			password: envMap["DB_PASSWORD"],
+			database: envMap["DB_DATABASE"],
+			sslMode:  envMap["DB_SSL_MODE"],
+			maxConnections: func() int {
+				m, err := strconv.Atoi(envMap["DB_MAX_CONNECTIONS"])
+				if err != nil {
+					log.Fatalf("load maxConnections error : %v", err)
+				}
+				return m
+			}(),
+		},
+		jwt: &jwt{
+			adminKey:  envMap["JWT_ADMIN_KEY"],
+			secertKey: envMap["JWT_SECERT_KEY"],
+			apiKey:    envMap["JWT_API_KEY"],
+			accessExpiresAt: func() int {
+				t, err := strconv.Atoi(envMap["JWT_ACCESS_EXPIRES"])
+				if err != nil {
+					log.Fatalf("load accessExpiresAt error : %v", err)
+				}
+				return t
+			}(),
+			refreshExpiresAt: func() int {
+				m, err := strconv.Atoi(envMap["JWT_REFRESH_EXPIRES"])
+				if err != nil {
+					log.Fatalf("load refreshExpiresAt error : %v", err)
+				}
+				return m
+			}(),
+		},
+	}
 }
 
-type db struct {
-	host           string
-	port           int
-	protocal       string
-	username       string
-	password       string
-	database       string
-	sslMode        string
-	maxConnections int
-}
-
-type jwt struct {
-	adminKey         string
-	secertKey        string
-	apiKey           string
-	accessExpiresAt  int
-	refreshExpiresAt int
+type IConfig interface {
+	App() IAppConfig
+	Db() IDbConfig
+	Jwt() IJwtConfig
 }
