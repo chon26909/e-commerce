d06b1073f31613c0a5afaf7747aef7782020405a
HiChon <59905927+chon26909@users.noreply.github.com>
2023-08-27 23:59:49 +0700

    create monitor handler

---
+++ modules/monitor/MonitorHandlers/monitor_handler.go
@@ -0,0 +1,31 @@
+package monitorhandlers
+
+import (
+	"github.com/chon26909/e-commerce/config"
+	"github.com/chon26909/e-commerce/modules/monitor"
+	"github.com/gofiber/fiber/v2"
+)
+
+type IMonitorHandler interface {
+	HealthCheck(c *fiber.Ctx) error
+}
+
+type monitorHandler struct {
+	config config.IConfig
+}
+
+func NewMonitorHandler(config config.IConfig) IMonitorHandler {
+	return &monitorHandler{
+		config: config,
+	}
+}
+
+func (h *monitorHandler) HealthCheck(c *fiber.Ctx) error {
+
+	res := &monitor.Monitor{
+		Name:    h.config.App().Name(),
+		Version: h.config.App().Version(),
+	}
+
+	return c.Status(fiber.StatusOK).JSON(res)
+}
