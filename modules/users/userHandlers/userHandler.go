4b460ed5e649a1e100b0e1ff888f0c3f21dfb937
HiChon <59905927+chon26909@users.noreply.github.com>
2023-08-31 23:32:36 +0700

    update handler

--- modules/users/userHandlers/userHandler.go
+++ modules/users/userHandlers/userHandler.go
@@ -1,1 +1,18 @@
 package userHandlers
+
+import (
+	"github.com/chon26909/e-commerce/config"
+	"github.com/chon26909/e-commerce/modules/users/userUsecases"
+)
+
+type IUserHandler interface {
+}
+
+type userHandler struct {
+	config      *config.IConfig
+	userUsecase userUsecases.IUserUsecase
+}
+
+func NewUserHandler(config *config.IConfig, userUsecase userUsecases.IUserUsecase) IUserHandler {
+	return &userHandler{config: config, userUsecase: userUsecase}
+}
