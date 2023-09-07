95444d5759c4411823537c6e61d68dd716c51709
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-07 23:30:01 +0700

    update usecase

--- modules/users/userUsecases/userUsecase.go
+++ modules/users/userUsecases/userUsecase.go
@@ -2,10 +2,12 @@ package userUsecases
 
 import (
 	"github.com/chon26909/e-commerce/config"
+	"github.com/chon26909/e-commerce/modules/users"
 	"github.com/chon26909/e-commerce/modules/users/userRepositories"
 )
 
 type IUserUsecase interface {
+	InsertCustomer(req *users.UserRegisterRequest) (*users.UserPassport, error)
 }
 
 type userUsecase struct {
@@ -15,4 +17,20 @@ type userUsecase struct {
 
 func NewUserUsecase(config *config.IConfig, userRepository userRepositories.IUserRepository) IUserUsecase {
 	return &userUsecase{config: config, userRepository: userRepository}
+}
+
+func (u *userUsecase) InsertCustomer(req *users.UserRegisterRequest) (*users.UserPassport, error) {
+
+	// hashing a password
+	if err := req.BcryptHash(); err != nil {
+		return nil, err
+	}
+
+	// insert user
+	result, err := u.userRepository.InsertUser(req, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
