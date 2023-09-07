0ae8afb60ea613132ad1244042e7c21b263d4082
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-07 23:30:48 +0700

    create module

--- modules/server/module.go
+++ modules/server/module.go
@@ -5,11 +5,15 @@ import (
 	"github.com/chon26909/e-commerce/modules/middlewares/middlewaresRepositories"
 	middlewaresUsecases "github.com/chon26909/e-commerce/modules/middlewares/middlewaresUseCases"
 	monitorhandlers "github.com/chon26909/e-commerce/modules/monitor/MonitorHandlers"
+	"github.com/chon26909/e-commerce/modules/users/userHandlers"
+	"github.com/chon26909/e-commerce/modules/users/userRepositories"
+	"github.com/chon26909/e-commerce/modules/users/userUsecases"
 	"github.com/gofiber/fiber/v2"
 )
 
 type IModuleFactory interface {
 	MonitorModule()
+	UsersModule()
 }
 
 type moduleFactory struct {
@@ -34,4 +38,14 @@ func (m *moduleFactory) MonitorModule() {
 	handler := monitorhandlers.NewMonitorHandler(m.server.config)
 
 	m.router.Get("/", handler.HealthCheck)
+}
+
+func (m *moduleFactory) UsersModule() {
+	repository := userRepositories.NewUserRepositories(m.server.db)
+	usecase := userUsecases.NewUserUsecase(&m.server.config, repository)
+	handler := userHandlers.NewUserHandler(&m.server.config, usecase)
+
+	router := m.router.Group("/users")
+
+	router.Post("/signup", handler.SignUpCustomer)
 }
