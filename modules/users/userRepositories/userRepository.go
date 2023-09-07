63eb6e894ba741fc2a75ac0e5f26391dfc88576d
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-07 21:06:45 +0700

    update user repository

--- modules/users/userRepositories/userRepository.go
+++ modules/users/userRepositories/userRepository.go
@@ -1,8 +1,12 @@
 package userRepositories
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/chon26909/e-commerce/modules/users"
+	"github.com/jmoiron/sqlx"
+)
 
 type IUserRepository interface {
+	InsertUser(req *users.UserRegisterRequest, isAdmin bool) (*users.UserPassport, error)
 }
 
 type userRepositories struct {
@@ -13,4 +17,9 @@ func NewUserRepositories(db *sqlx.DB) IUserRepository {
 	return &userRepositories{
 		db: db,
 	}
+}
+
+// InsertUser implements IUserRepository.
+func (r *userRepositories) InsertUser(req *users.UserRegisterRequest, isAdmin bool) (*users.UserPassport, error) {
+	panic("unimplemented")
 }
