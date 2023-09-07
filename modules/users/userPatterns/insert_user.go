7717403f4db331880a77dd3c60ca56c6a48fb8ed
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-07 21:06:17 +0700

    rename

--- modules/users/userPatterns/insert_user.go
+++
@@ -1,9 +0,0 @@
-package userPatterns
-
-import "github.com/chon26909/e-commerce/modules/users"
-
-type IInsertUser interface {
-	Customer() (IInsertUser, error)
-	Admin() (IInsertUser, error)
-	Result(*users.User) error
-}
