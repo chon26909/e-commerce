9376c3ce638b499cb0977503597fac28a22aa78c
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-09 22:59:38 +0700

    rename

---
+++ modules/users/users.go
@@ -0,0 +1,65 @@
+package users
+
+import (
+	"fmt"
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Id       string `db:"id" json:"id"`
+	Email    string `db:"email" json:"email"`
+	UserName string `db:"user_name" json:"user_name"`
+	RoleId   int    `db:"role_id" json:"role_id"`
+}
+
+type UserRegisterRequest struct {
+	Email    string `db:"email" json:"email"`
+	Password string `db:"password" json:"password"`
+	Username string `db:"username" json:"username"`
+}
+
+type UserCredential struct {
+	Email    string `db:"email" json:"email"`
+	Password string `db:"password" json:"password"`
+}
+
+type UserCredentialCheck struct {
+	Id       string `db:"id"`
+	Email    string `db:"email"`
+	Password string `db:"password"`
+	Username string `db:"username"`
+	RoleId   string `db:"role_id"`
+}
+
+func (obj *UserRegisterRequest) BcryptHash() error {
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(obj.Password), 10)
+	if err != nil {
+		return fmt.Errorf("hashed password failed: %v", err)
+	}
+	obj.Password = string(hashedPassword)
+
+	return nil
+}
+
+func (obj *UserRegisterRequest) IsEmail() bool {
+	match, err := regexp.MatchString(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`, obj.Email)
+	if err != nil {
+		return false
+	}
+
+	return match
+}
+
+type UserPassport struct {
+	User  *User  `json:"user"`
+	Token string `json:"token"`
+}
+
+type UserToken struct {
+	Id           string `db:"id" json:"id"`
+	AccessToken  string `db:"access_token" json:"access_token"`
+	RefreshToken string `db:"refresh_token" json:"refresh_token"`
+}
