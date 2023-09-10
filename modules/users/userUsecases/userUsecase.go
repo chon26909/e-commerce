3e08787f19ee30919fd555b8a57130d8ecd1bed8
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-10 23:30:50 +0700

    use case

--- modules/users/userUsecases/userUsecase.go
+++ modules/users/userUsecases/userUsecase.go
@@ -1,13 +1,17 @@
 package userUsecases
 
 import (
+	"fmt"
+
 	"github.com/chon26909/e-commerce/config"
 	"github.com/chon26909/e-commerce/modules/users"
 	"github.com/chon26909/e-commerce/modules/users/userRepositories"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type IUserUsecase interface {
 	InsertCustomer(req *users.UserRegisterRequest) (*users.UserPassport, error)
+	GetPassport(req *users.UserCredential) (*users.UserPassport, error)
 }
 
 type userUsecase struct {
@@ -33,4 +37,27 @@ func (u *userUsecase) InsertCustomer(req *users.UserRegisterRequest) (*users.Use
 	}
 
 	return result, nil
+}
+
+func (u *userUsecase) GetPassport(req *users.UserCredential) (*users.UserPassport, error) {
+	user, err := u.userRepository.FindOneUserbyEmail(req.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return nil, fmt.Errorf("failed to compare password")
+	}
+
+	passport := &users.UserPassport{
+		User: &users.User{
+			Id:       user.Id,
+			Email:    user.Email,
+			UserName: user.Username,
+			RoleId:   user.RoleId,
+		},
+		Token: nil,
+	}
+
+	return passport, nil
 }
