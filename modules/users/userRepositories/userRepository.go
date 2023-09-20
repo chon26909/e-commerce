a9ece42a6c3f4371d193a506e646ecfdca8b10f9
HiChon <59905927+chon26909@users.noreply.github.com>
2023-09-20 21:26:45 +0700

    r

--- modules/users/userRepositories/userRepository.go
+++ modules/users/userRepositories/userRepository.go
@@ -100,3 +100,35 @@ func (r *userRepository) InsertOAuth(req *users.UserPassport) error {
 
 	return nil
 }
+
+func (r *userRepository) FindOneOauth(refreshToken string) (*users.Oauth, error) {
+
+	query := `
+		SELECT 
+			"id",
+			"user_id"
+		FROM "oauth"
+		WHERE "refresh_token" = $1;
+	`
+
+	oauth := new(users.Oauth)
+	if err := r.db.Get(oauth, query, refreshToken); err != nil {
+		return nil, fmt.Errorf("oauth not found: %v", err)
+	}
+	return oauth, nil
+}
+
+func (r *userRepository) UpdateOauth(req *users.UserToken) error {
+
+	query := `
+		UPDATE 	"oauth" 
+		SET 	"access_token" = :access_token,
+				"refresh_token" = :refresh_token
+		WHERE "id" = :id;
+	`
+	if _, err := r.db.NamedExecContext(context.Background(), query, req); err != nil {
+		return fmt.Errorf("update oauth failed: %v", err)
+	}
+
+	return nil
+}
